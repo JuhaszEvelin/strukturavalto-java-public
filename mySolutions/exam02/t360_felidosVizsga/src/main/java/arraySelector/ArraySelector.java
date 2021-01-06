059de16f056b7b9500093cd5de9163de41c80246
Juhasz Evelin <juhaaszevelin@gmail.com>
2021-01-06 22:37:00 +0100

    ArraySelector implemented

--- mySolutions/exam02/t360_felidosVizsga/src/main/java/arraySelector/ArraySelector.java
+++ mySolutions/exam02/t360_felidosVizsga/src/main/java/arraySelector/ArraySelector.java
@@ -2,6 +2,40 @@ package arraySelector;
 
 public class ArraySelector {
 
+    /**
+     * Írj egy olyan metódust az ArraySelector osztályban, mely egy kapott tömbből minden második elemet kiválogat,
+     * és egy szövegként összerak, szögletes zárójelek között!
+     * <p>
+     * A páros megállapítása index szerint működik. Azaz az első, nulla indexű elem páros,
+     * a második, azaz egyes indexű elem páratlan, stb.
+     * <p>
+     * Ha nulla elemű a tömb, akkor üres stringet adjon vissza!
+     */
 
+    public static String selectEvens(int[] array) {
+        String result = "";
+
+        if (array.length != 0) {
+            result += "[";
+            for (int i = 0; i < array.length; i++) {
+
+                if (i == 0) {
+                    result += array[i];
+                } else if (i % 2 == 0) {
+                    result += ", " + array[i];
+                }
+            }
+            result += "]";
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(selectEvens(new int[]{1}));
+
+    }
 
 }
