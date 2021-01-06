f63c89241b173b602965240501c7b4fcc5c4ffce
Juhasz Evelin <juhaaszevelin@gmail.com>
2021-01-06 22:42:31 +0100

    LottoReaderTest implemented, cv started

---
+++ mySolutions/exam02/t360_felidosVizsga/src/test/java/cv/LottoReaderTest.java
@@ -0,0 +1,16 @@
+package cv;
+
+import java.io.InputStream;
+
+public class LottoReaderTest {
+
+    @Test
+    public void testCreate() {
+        InputStream inputStream = LottoReader.class.getResourceAsStream("/otos.csv");
+        LottoReader lottoReader = new LottoReader(inputStream);
+        assertEquals(190, lottoReader.getNumber(1));
+        assertEquals(148, lottoReader.getNumber(5));
+        assertEquals(185, lottoReader.getNumber(22));
+        assertEquals(172, lottoReader.getNumber(90));
+    }
+}
